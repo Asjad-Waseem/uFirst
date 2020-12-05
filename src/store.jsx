cb33d2075e7755ba5b3e0eb4dbe820a8fe721147
Asjad-Waseem <70429177+Asjad-Waseem@users.noreply.github.com>
2020-12-06 01:27:49 +0500

    Update store.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -6,21 +6,12 @@ import { persistStore } from 'redux-persist';
 const initialState = {};
 const middleware = [thunk];
 
-let enhancer;
-
- if (process.env.NODE_ENV === 'production') {
-    enhancer = applyMiddleware(...middleware);
-  } else {
-    enhancer = compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : noop => noop
-    );
-  }
-
-export const store = createStore(rootReducer, initialState, enhancer);
+export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export const persistor = persistStore(store);
 export default {store, persistor};
+
+
 
 
 
